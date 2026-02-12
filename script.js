9e49327f8016555457861bdc46d73a70c4c2b626
Alex28322 <sander2832@gmail.com>
2026-02-12 15:30:17 +0200

    async version

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 
 
-function fetchPockemon () {
+async function fetchPockemon () {
     const errormessage = document.getElementById("error-text")
-const pockemonName = document.getElementById("pockemonName").value    
+const pockemonName = document.getElementById("pockemonName").value.toLowerCase()    
 const pockemonImgHTML = document.getElementById("pockemonSprite");
 const pockemonWeightHTML = document.querySelector(".pockemon__wieght");
 const pockemonTypeHTML = document.querySelector(".pockemon__type");
@@ -13,15 +13,14 @@ errormessage.style.display = "none"
   pockemonImgHTML.style.display = "none"
 pockemonWeightHTML.textContent = ""
 pockemonTypeHTML.textContent = ""
-fetch(`https://pokeapi.co/api/v2/pokemon/${pockemonName}`)
-.then( response => {
-        if(!response.ok){
-            throw new Error("server error");    
-        }
-        return response.json()
-    })
-    .then( data => {
-        const pockemonImg = data.sprites.front_default;
+
+try{
+    const responce = await fetch(`https://pokeapi.co/api/v2/pokemon/${pockemonName}`);
+    if(!responce.ok){
+        throw new Error ("server error")
+    }
+    const data = await responce.json();
+    const pockemonImg = data.sprites.front_default;
         
         pockemonImgHTML.src = pockemonImg;
         pockemonImgHTML.style.display = "block";
@@ -31,11 +30,37 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${pockemonName}`)
         const pockemonType = data.types[0].type.name;
         
         pockemonTypeHTML.textContent = `Pockemon type: ${pockemonType}`
-    })
-    .catch( error => {
-        console.error(error)
-        
+}
+catch (error){
+console.error(error)
          pockemonImgHTML.style.display = "none";
         errormessage.style.display = "block"
         errormessage.textContent = "The error has occured"
-    })}
+}}
+
+// fetch(`https://pokeapi.co/api/v2/pokemon/${pockemonName}`)
+// .then( response => {
+//         if(!response.ok){
+//             throw new Error("server error");    
+//         }
+//         return response.json()
+//     })
+//     .then( data => {
+//         const pockemonImg = data.sprites.front_default;
+        
+//         pockemonImgHTML.src = pockemonImg;
+//         pockemonImgHTML.style.display = "block";
+//         const pockemonWeight = data.weight;
+        
+//         pockemonWeightHTML.textContent = `Pockemon weight: ${pockemonWeight}`;
+//         const pockemonType = data.types[0].type.name;
+        
+//         pockemonTypeHTML.textContent = `Pockemon type: ${pockemonType}`
+//     })
+//     .catch( error => {
+//         console.error(error)
+        
+//          pockemonImgHTML.style.display = "none";
+//         errormessage.style.display = "block"
+//         errormessage.textContent = "The error has occured"
+//     })}
